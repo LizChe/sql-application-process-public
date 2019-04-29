d370ff7d54b1c997274f4ce9fc54119e78870df7
Liz <pachaffy@gmail.com>
2019-04-29 13:18:03 +0200

    Initialize DaoException.java

---
+++ src/main/java/com/codecool/application_process/dao/DatabaseConnector.java
@@ -0,0 +1,26 @@
+package com.codecool.application_process.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//public class DatabaseConnection {
+//
+//    public Connection getConnection() {
+//        Connection connection = null;
+//        String URL = "jdbc:postgresql://localhost:5432/application-process";
+//        String USER = "postgres";
+//        String PASSWORD = "123";
+//
+//        try {
+//            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return connection;
+//    }
+//}
+
+public class DatabaseConnector {
+
+}
