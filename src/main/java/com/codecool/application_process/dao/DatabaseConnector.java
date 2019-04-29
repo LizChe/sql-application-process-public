09a5358a81fb8cac8e50e75fb19f5a5d5347773f
Liz <pachaffy@gmail.com>
2019-04-29 23:31:39 +0200

    Refactor DatabaseConnection
    - Change name to DatabaseConnector
    - Implement Connection Pool with Hikari
    - Change class' and class' methods access modificator from puplic to package-private

--- src/main/java/com/codecool/application_process/dao/DatabaseConnector.java
+++ src/main/java/com/codecool/application_process/dao/DatabaseConnector.java
@@ -1,26 +1,32 @@
 package com.codecool.application_process.dao;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
-//public class DatabaseConnection {
-//
-//    public Connection getConnection() {
-//        Connection connection = null;
-//        String URL = "jdbc:postgresql://localhost:5432/application-process";
-//        String USER = "postgres";
-//        String PASSWORD = "123";
-//
-//        try {
-//            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return connection;
-//    }
-//}
+class DatabaseConnector {
 
-public class DatabaseConnector {
+    private static HikariConfig config = new HikariConfig();
+    private static HikariDataSource dataSource;
+
+    static {
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/application-process");
+        config.setUsername("postgres");
+        config.setPassword("123");
+        config.addDataSourceProperty( "cachePrepStmts" , "true" );
+        config.addDataSourceProperty( "prepStmtCacheSize" , "250" );
+        config.addDataSourceProperty( "prepStmtCacheSqlLimit" , "2048" );
+        dataSource = new HikariDataSource(config);
+    }
+
+    private DatabaseConnector() {
+
+    }
+
+    static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
 
 }
