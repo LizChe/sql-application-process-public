beb291b6315ea6934db88f173836ebd4b9f0fbe3
Liz <pachaffy@gmail.com>
2019-05-03 00:28:23 +0200

    Add Search option in Controller

--- src/main/java/com/codecool/application_process/controller/Controller.java
+++ src/main/java/com/codecool/application_process/controller/Controller.java
@@ -1,5 +1,7 @@
 package com.codecool.application_process.controller;
 
+import com.codecool.application_process.service.SearchService;
+
 import com.codecool.application_process.view.View;
 
 public class Controller {
@@ -7,19 +9,21 @@ public class Controller {
     private View view;
     private MentorController mentorController;
     private ApplicantController applicantController;
+    private SearchService searchService;
     private boolean isRunning;
 
     public Controller() {
         view = new View();
         mentorController = new MentorController();
         applicantController = new ApplicantController();
+        searchService = new SearchService();
         isRunning = true;
     }
 
     public void run() {
         isRunning = true;
+        view.clearScreen();
         while (isRunning) {
-            view.clearScreen();
             view.displayLogo();
             view.displayMenu();
            handleController();
@@ -35,6 +39,9 @@ public class Controller {
                 break;
             case 2:
                 applicantController.handleApplicantController();
+                break;
+            case 3:
+                searchService.getSearchResult();
                 break;
             case 0:
                 isRunning = false;
