7fb529a16e43a59b923b86e29ce7cec5a61c4539
Liz <pachaffy@gmail.com>
2019-05-18 16:13:57 +0200

    Change type of exception from Runtime to IllegalArgument in DaoException

--- src/main/java/com/codecool/application_process/dao/DaoException.java
+++ src/main/java/com/codecool/application_process/dao/DaoException.java
@@ -1,8 +1,8 @@
 package com.codecool.application_process.dao;
 
-public class DaoException extends RuntimeException {
+public class DaoException extends IllegalArgumentException {
 
-    public DaoException(String errorMessage) {
+    DaoException(String errorMessage) {
         super(errorMessage);
     }
 }
